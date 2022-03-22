ce5aff547d481bda7aa3eccf90fb65a907d9259b
aquila-zyy <aquila.zyy@gmail.com>
2022-03-22 08:37:35 -0400

    Make BackGround3D reusable

--- frontend/src/routes/LRFromWrapper.tsx
+++ frontend/src/routes/LRFromWrapper.tsx
@@ -3,7 +3,19 @@ import LoginRegisterForm from "../components/LoginRegisterForm";
 import BackGround3D from "../components/3d/BackGround3D";
 
 const LRFormWrapper = function () {
-    return <BackGround3D component={<LoginRegisterForm />} />;
+    return (
+        <BackGround3D
+            paths={[
+                "/assets/right.png",
+                "/assets/left.png",
+                "/assets/top.png",
+                "/assets/bottom.png",
+                "/assets/front.png",
+                "/assets/back.png",
+            ]}
+            component={<LoginRegisterForm />}
+        />
+    );
 };
 
 export default LRFormWrapper;
